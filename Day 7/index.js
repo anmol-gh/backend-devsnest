6daffebb65e06ae5ce822d33aff2635acc252b70
noob-anmol <anmolghai50@gmail.com>
2021-09-28 23:28:01 +0530

    added content in day07

--- Day 7/index.js
+++ Day 7/index.js
@@ -1,12 +1,24 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 //CRUD || CREATE -> POST, READ -> GET, UPDATE -> PUT, DELETE -> DELETE
 
-//Creating Routes
-app.get('/',(req,res) =>{
-    res.sendStatus(200).send("hi")
-    res.send("hi")
-})
-app.listen(5000)
+//How to Create Routes?
+
+// app.get('/',(req,res) =>{
+//     res.sendStatus(200).send("hi")
+//     res.send("hi")
+// })
+// app.listen(5000)
+
+//Check Regex in url 
+
+//: refers to variable 
+
+app.get("/user/:userId/book/:bookId", (req, res) => {
+console.log(req.query);
+  res.send(req.params.bookId);
+});
+
+app.listen(5000);
